e7828ee4999a32635c39d68473fd7ffae5f64f4a
ProdigyElite49 <crimsonsreality@gmail.com>
2020-05-07 20:05:37 -0400

    Audio

--- Assets/Scripts/UI/PauseScript.cs
+++ Assets/Scripts/UI/PauseScript.cs
@@ -11,7 +11,7 @@ public class PauseScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape) || InputManager.GetButtonPressed(GamepadButton.Start))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (GameIsPaused)
             {
